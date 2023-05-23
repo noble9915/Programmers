6e9ef64c660549da34b340f508b0bb183119d377
Min Ria <noble9915@gmail.com>
2023-05-23 14:15:07 +0900

    no message

---
+++ a와 b 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.print("a = "+a);
+        System.out.println();
+        System.out.print("b = "+b);
+        System.out.println();
+    }
+}
